1caf156a389c68ea2170269e81e88ae1d24f461a
yiqijiang17 <46921024+yiqijiang17@users.noreply.github.com>
2022-09-29 18:58:35 +0800

    update usage

--- diff_testing/main.py
+++ diff_testing/main.py
@@ -10,8 +10,11 @@ import os
     --dsf    <str> input file from subplatform
     --ann    <str> input file of group info
     --groupid  <str> column name used for grouping, default: phenotype
-    --method <str> method for visualization [PCA/PCoA]
-    --maxk <int> maxinum number of clusters to find the optimal k. default:10
+    --method <str> method for testing [wilcox.test/t.test/kruskal.test/aov]
+    --adjust <str> method for adjusting p-values, default: fdr. [fdr/BH/hochberg/holm/bonferroni/hommel]
+    --mean_cutoff <float> threshold of mean value to filter taxonomy, at least one group should have a mean relative abundance greater than this threshold, defualt: 0
+    --pvalue_cutoff <float> threshold of occurence to filter taxonomy, the taxonomy should at least exist in the threshold ratio of samples of certain group, defualt: 0.1
+
 '''
 
 script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'diff.R')
